b25d5bbef50632a1b8828496088c446cfa17aa9d
Chris Horne <chrhorne@deloitte.com>
2024-03-07 11:25:09 -0700

    Completed exercise.

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 // Write your code here
+const lightbulb1 = document.querySelector("#lightbulb1");
+const lightbulb2 = document.querySelector("#lightbulb2");
+const lightbulb3 = document.querySelector("#lightbulb3");
+const subtitle = document.querySelector(".subtitle");
+
+let count = 0;
+
+lightbulb1.addEventListener("click", function(){
+    count++
+    subtitle.innerHTML = `You've clicked the lights ${count} time(s)`;
+    this.classList.toggle("active")
+})
+lightbulb2.addEventListener("click", function(){
+    count++
+    subtitle.innerHTML = `You've clicked the lights ${count} time(s)`;
+    this.classList.toggle("active")
+})
+lightbulb3.addEventListener("click", function(){
+    count++
+    subtitle.innerHTML = `You've clicked the lights ${count} time(s)`;
+    this.classList.toggle("active")
+})
+
